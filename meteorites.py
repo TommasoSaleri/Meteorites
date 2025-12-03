681566ecf281947e9e0a0337add56def5b5e6946
Tommaso Saleri <tommaso.saleri@gmail.com>
2025-12-03 13:09:20 +0100

    Add files via upload
    Suddivisione dati relativi alla massa e all'anno di caduta di ogni meteorite

--- meteorites.py
+++ meteorites.py
@@ -9,3 +9,22 @@ with open(file_name, "r", encoding="utf8") as file:
 #print(type(stringa))
 dati_meteoriti = json.loads(stringa)
 #print(type(dati_meteoriti))
+
+#print(dati_meteoriti[0].keys())
+
+masse = []
+for elem in dati_meteoriti:
+    try:
+        masse.append(elem["mass"])
+    except:
+        continue
+
+anni = []
+for elem in dati_meteoriti:
+    try:
+        anni.append(elem["year"])
+    except:
+        continue
+
+#print(masse)
+#print(anni)
