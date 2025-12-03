4a62bd69f4f149c7f6059d35b0cf91a1712aa56a
Tommaso Saleri <tommaso.saleri@gmail.com>
2025-12-03 13:28:23 +0100

    Add files via upload
    Attività di Laboratorio 2: Visualizzazione della Distribuzione delle Masse

--- meteorites.py
+++ meteorites.py
@@ -14,3 +14,23 @@ print(dati_meteoriti[0].keys())
 
 for elem in range(0, 5):
     print(dati_meteoriti[elem],"\n")
+
+masse = []
+anni = []
+for elem in dati_meteoriti:
+    try:
+        masse.append(elem["mass"])
+        anni.append(elem["year"])
+    except:
+        continue
+
+#print(masse)
+#print(anni)
+
+plt.figure(figsize=(10, 6))
+plt.hist(masse, bins=15, edgecolor='black', log=True)
+plt.xlabel("Massa (g)")
+plt.ylabel("Frequenza (Scala Logaritmica)")
+plt.title("Distribuzione delle Masse dei Meteoriti")
+plt.grid(axis='y', alpha=0.75)
+plt.show()
